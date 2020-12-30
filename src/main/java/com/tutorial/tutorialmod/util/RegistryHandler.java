af74d8ea5c8044b141ba733d4d781877111d42f5
RunTime256 <runtime256@gmail.com>
2020-12-29 19:29:45 -0600

    Ruby tools

--- src/main/java/com/tutorial/tutorialmod/util/RegistryHandler.java
+++ src/main/java/com/tutorial/tutorialmod/util/RegistryHandler.java
@@ -5,8 +5,9 @@ import com.tutorial.tutorialmod.blocks.BlockItemBase;
 import com.tutorial.tutorialmod.blocks.RubyBlock;
 import com.tutorial.tutorialmod.blocks.RubyOre;
 import com.tutorial.tutorialmod.items.ItemBase;
+import com.tutorial.tutorialmod.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,11 +18,26 @@ public class RegistryHandler
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+    // Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
     public static final RegistryObject<Block> RUBY_ORE = BLOCKS.register("ruby_ore", RubyOre::new);
+
+    // Items
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
     public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
     public static final RegistryObject<Item> RUBY_ORE_ITEM = ITEMS.register("ruby_ore", () -> new BlockItemBase(RUBY_ORE.get()));
+
+    // Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group(TutorialMod.TAB)));
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(ModItemTier.RUBY, 0, -2.8f, new Item.Properties().group(TutorialMod.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY, 0.5f, -3.0f, new Item.Properties().group(TutorialMod.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 5, -3.1f, new Item.Properties().group(TutorialMod.TAB)));
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
+            new HoeItem(ModItemTier.RUBY, 0, -1.8f, new Item.Properties().group(TutorialMod.TAB)));
 
     private RegistryHandler()
     {
